22f411cdb27676a9310ea9a6ee4da604a2ca1df1
r0ymanesco <tt2114@ic.ac.uk>
2020-02-23 16:24:40 +0000

    debug

--- train.py
+++ train.py
@@ -14,8 +14,8 @@ from dataset import DataFolder
 import torch.utils.data as data
 from util import EarlyStopping, save_nets, save_predictions
 
-train_batch_size = 16
-eval_batch_size = 16
+train_batch_size = 1
+eval_batch_size = 1
 epochs = 100
 lr = 0.001
 
@@ -140,7 +140,7 @@ with torch.no_grad():
 
         all_loss.append(loss.item())
 
-        ipdb.set_trace()
+        
 
         pred_mask = torch.argmax(F.softmax(pred, dim=1), dim=1)
         pred_mask = torch.chunk(pred_mask, chunks=eval_batch_size, dim=0)
